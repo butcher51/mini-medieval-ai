bb2799c792234c7be2fb5fed4d66df8c50d40c43
Gabor Meszaros <gmeszaros@foss.dk>
2025-03-06 12:38:41 +0100

    tiled

---
+++ tiled/Overworld.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Overworld" tilewidth="8" tileheight="8" tilecount="2232" columns="24">
+ <image source="../../../mini_medival_pack/Mini-Medieval-8x8/Overworld.png" width="192" height="744"/>
+</tileset>
