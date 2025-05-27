2781ac509cc90123461240452adbadac06bf27a4
Gabor Meszaros <gmeszaros@foss.dk>
2025-05-27 14:27:01 +0200

    Map update

--- tiled/Overworld.tsx
+++ tiled/Overworld.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.11" tiledversion="1.11.2" name="Overworld" tilewidth="8" tileheight="8" tilecount="2232" columns="24">
- <image source="../../../mini_medival_pack/Mini-Medieval-8x8/Overworld.png" width="192" height="744"/>
+ <image source="../assets/overworld.png" width="192" height="744"/>
 </tileset>
