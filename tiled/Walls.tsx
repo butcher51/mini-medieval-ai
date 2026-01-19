8a5a1ec4c6a0a99704070ca978ce267ec9887f5b
Meszaros Gabor <gabor.meszaros@gmail.com>
2026-01-19 22:53:04 +0100

    walls-part-1

---
+++ tiled/Walls.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="Walls" tilewidth="8" tileheight="8" tilecount="1392" columns="29">
+ <image source="../../../mini_medival_pack/Mini-Medieval-8x8/Walls.png" width="232" height="384"/>
+</tileset>
